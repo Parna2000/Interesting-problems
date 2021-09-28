f7abc5baf3365b6d2d5892aa13ff61f56e3143e9
Parna2000 <21ms0093@mc.iitism.ac.in>
2021-09-28 22:35:25 +0530

    Easy problems

---
+++ dish.c
@@ -0,0 +1,4 @@
+// Chef will have N guests in his house today. He wants to serve atleast one dish to each one of the N guests. Chef can make two types of dishes. He needs one fruit and one vegetable to make first type of dish and one vegetable and one fish to make second type of dish.Now Chef has A fruits, B vegetables and C fishes in his house. Can he prepare at least N dishes in total?
+
+// algo: B>=N
+// A+C>=N
---
+++ vaccine.c
@@ -0,0 +1,1 @@
+// Chef has taken his first dose of vaccine D days ago. The prescribed range for taking the dose is between L and R days(both inclusive) from the date of administration of the first dose. Determine if the Chef is too early, too late, or in the correct range for taking his dose.
